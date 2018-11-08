138a16ad472bb7e83e8292150c231839f4716ef1
Johan Tan <johanaldrictan@gmail.com>
2018-11-08 08:30:53 -0800

    Completed MarchingSquares algorithm for setting up mesh generation Switched casing styles to fit C# standard

--- Assets/MapGenerator.cs
+++ Assets/MapGenerator.cs
@@ -5,17 +5,17 @@ using System;
 
 public class MapGenerator : MonoBehaviour {
 
-    public int width;
-    public int height;
+    public int Width;
+    public int Height;
 
-    public string seed;
-    public bool useRandomSeed;
+    public string Seed;
+    public bool UseRandomSeed;
     public int SmoothFactor = 5;
 
     [Range(0,100)]
-    public int randomFillPercent;
+    public int RandomFillPercent;
 
-    int[,] map;
+    int[,] Map;
     //coordinate with 0 is empty coordinate with 1 has a wall
 
     void Start()
@@ -31,47 +31,49 @@ public class MapGenerator : MonoBehaviour {
     }
     void GenerateMap()
     {
-        map = new int[width, height];
+        Map = new int[Width, Height];
         RandomFillMap();
         for(int i = 0; i< SmoothFactor; i++)
         {
             SmoothMap();
         }
+        MeshGenerator meshGen = GetComponent<MeshGenerator>();
+        meshGen.GenerateMesh(Map, 1);
     }
 
     void RandomFillMap()
     {
-        if (useRandomSeed)
+        if (UseRandomSeed)
         {
-            seed = Time.time.ToString();
+            Seed = Time.time.ToString();
         }
-        System.Random pseudoRandom = new System.Random(seed.GetHashCode());
-        for(int x = 0; x < width; x++)
+        System.Random pseudoRandom = new System.Random(Seed.GetHashCode());
+        for(int x = 0; x < Width; x++)
         {
-            for(int y = 0; y < height; y++)
+            for(int y = 0; y < Height; y++)
             {
-                if (x == 0 || x == width - 1 || y == 0 || y == height - 1)
+                if (x == 0 || x == Width - 1 || y == 0 || y == Height - 1)
                 {
-                    map[x, y] = 1;
+                    Map[x, y] = 1;
                 }
                 else
                 {
-                    map[x, y] = pseudoRandom.Next(0, 100) < randomFillPercent ? 1 : 0;
+                    Map[x, y] = pseudoRandom.Next(0, 100) < RandomFillPercent ? 1 : 0;
                 }
             }
         }
     }
     void SmoothMap()
     {
-        for (int x = 0; x < width; x++)
+        for (int x = 0; x < Width; x++)
         {
-            for (int y = 0; y < height; y++)
+            for (int y = 0; y < Height; y++)
             {
                 int neighbourWallTiles = GetSurroundingWallCount(x, y);
                 if (neighbourWallTiles > 4)
-                    map[x, y] = 1;
+                    Map[x, y] = 1;
                 else if (neighbourWallTiles < 4)
-                    map[x, y] = 0;
+                    Map[x, y] = 0;
             }
         }
     }
@@ -82,11 +84,11 @@ public class MapGenerator : MonoBehaviour {
         {
             for (int neighbourY = gridY - 1; neighbourY <= gridY + 1; neighbourY++)
             {
-                if (neighbourX >= 0 && neighbourX < width && neighbourY >= 0 && neighbourY < height)
+                if (neighbourX >= 0 && neighbourX < Width && neighbourY >= 0 && neighbourY < Height)
                 {
                     if (neighbourX != gridX || neighbourY != gridY)
                     {
-                        wallCount += map[neighbourX, neighbourY];
+                        wallCount += Map[neighbourX, neighbourY];
                     }
                 }
                 else
@@ -99,17 +101,17 @@ public class MapGenerator : MonoBehaviour {
     }
     private void OnDrawGizmos()
     {
-        if(map != null)
-        {
-            for(int x = 0; x < width; x++)
-            {
-                for(int y = 0; y < height; y++)
-                {
-                    Gizmos.color = (map[x, y] == 1) ? Color.black : Color.white;
-                    Vector3 pos = new Vector3(-width / 2 + x + .5f, 0, -height / 2 + y + .5f);
-                    Gizmos.DrawCube(pos, Vector3.one);
-                }
-            }
-        }
+        //if(Map != null)
+        //{
+        //    for(int x = 0; x < Width; x++)
+        //    {
+        //        for(int y = 0; y < Height; y++)
+        //        {
+        //            Gizmos.color = (Map[x, y] == 1) ? Color.black : Color.white;
+        //            Vector3 pos = new Vector3(-Width / 2 + x + .5f, 0, -Height / 2 + y + .5f);
+        //            Gizmos.DrawCube(pos, Vector3.one);
+        //        }
+        //    }
+        //}
     }
 }
--- Assets/MeshGenerator.cs
+++ Assets/MeshGenerator.cs
@@ -4,26 +4,116 @@ using UnityEngine;
 
 public class MeshGenerator : MonoBehaviour {
 
+    public SquareGrid _SquareGrid;
+    
+    public void GenerateMesh(int[,] map , float squareSize)
+    {
+        _SquareGrid = new SquareGrid(map, squareSize);
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (_SquareGrid != null)
+        {
+            for (int x = 0; x < _SquareGrid.Squares.GetLength(0); x++)
+            {
+                for (int y = 0; y < _SquareGrid.Squares.GetLength(1); y++)
+                {
+                    Gizmos.color = (_SquareGrid.Squares[x, y].TopLeft.Active) ? Color.black : Color.white;
+                    Gizmos.DrawCube(_SquareGrid.Squares[x, y].TopLeft.Position, Vector3.one * .4f);
+
+                    Gizmos.color = (_SquareGrid.Squares[x, y].TopRight.Active) ? Color.black : Color.white;
+                    Gizmos.DrawCube(_SquareGrid.Squares[x, y].TopRight.Position, Vector3.one * .4f);
+
+                    Gizmos.color = (_SquareGrid.Squares[x, y].BottomRight.Active) ? Color.black : Color.white;
+                    Gizmos.DrawCube(_SquareGrid.Squares[x, y].BottomRight.Position, Vector3.one * .4f);
+
+                    Gizmos.color = (_SquareGrid.Squares[x, y].BottomLeft.Active) ? Color.black : Color.white;
+                    Gizmos.DrawCube(_SquareGrid.Squares[x, y].BottomLeft.Position, Vector3.one * .4f);
+
+
+                    Gizmos.color = Color.grey;
+                    Gizmos.DrawCube(_SquareGrid.Squares[x, y].CenterTop.Position, Vector3.one * .15f);
+                    Gizmos.DrawCube(_SquareGrid.Squares[x, y].CenterRight.Position, Vector3.one * .15f);
+                    Gizmos.DrawCube(_SquareGrid.Squares[x, y].CenterBottom.Position, Vector3.one * .15f);
+                    Gizmos.DrawCube(_SquareGrid.Squares[x, y].CenterLeft.Position, Vector3.one * .15f);
+                }
+            }
+        }
+    }
+
+    public class SquareGrid
+    {
+        public Square[,] Squares;
+
+        public SquareGrid(int[,] map, float squareSize)
+        {
+            int nodeCountX = map.GetLength(0);
+            int nodeCountY = map.GetLength(1);
+            float mapWidth = nodeCountX * squareSize;
+            float mapHeight = nodeCountY * squareSize;
+
+            ControlNode[,] controlNodes = new ControlNode[nodeCountX, nodeCountY];
+            for (int x = 0; x < nodeCountX; x++)
+            {
+                for (int y = 0; y < nodeCountY; y++)
+                {
+                    Vector3 pos = new Vector3(-mapWidth / 2 + x * squareSize + squareSize / 2, 0,
+                        -mapWidth / 2 + y * squareSize + squareSize / 2);
+                    controlNodes[x, y] = new ControlNode(pos, map[x, y] == 1, squareSize);
+                }
+            }
+            Squares = new Square[nodeCountX - 1, nodeCountY - 1];
+            for (int x = 0; x < nodeCountX - 1; x++)
+            {
+                for (int y = 0; y < nodeCountY - 1; y++)
+                {
+                    Squares[x,y] =  new Square(controlNodes[x, y + 1], controlNodes[x + 1, y + 1], controlNodes[x + 1, y], controlNodes[x, y]);
+                }
+            }
+        }
+    }
+
+    public class Square
+    {
+        public ControlNode TopLeft, TopRight, BottomRight, BottomLeft;
+        public Node CenterTop, CenterRight, CenterBottom, CenterLeft;
+
+        public Square(ControlNode topLeft, ControlNode topRight, ControlNode bottomRight, ControlNode bottomLeft)
+        {
+            TopLeft = topLeft;
+            TopRight = topRight;
+            BottomRight = bottomRight;
+            BottomLeft = bottomLeft;
+
+            CenterTop = topLeft.Right;
+            CenterRight = bottomRight.Above;
+            CenterBottom = bottomLeft.Right;
+            CenterLeft = bottomLeft.Above;
+        }
+    }
+
     public class Node
     {
-        public Vector3 position;
-        public int vertexIndex = -1;
+        public Vector3 Position;
+        public int VertexIndex = -1;
 
-        public Node(Vector3 _pos)
+        public Node(Vector3 pos)
         {
-            position = _pos;
+            Position = pos;
         }
     }
 
     public class ControlNode : Node
     {
-        public bool active;
-        public Node above, right;
+        public bool Active;
+        public Node Above, Right;
 
-        public ControlNode(Vector3 _pos, bool _active, float squareSize) : base(_pos)
+        public ControlNode(Vector3 pos, bool active, float squareSize) : base(pos)
         {
-            active = _active;
-
+            Active = active;
+            Above = new Node(Position + Vector3.forward * squareSize / 2f);
+            Right = new Node(Position + Vector3.right * squareSize / 2f);
         }
     }
 	// Use this for initialization
