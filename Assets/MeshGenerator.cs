949774cbfc94ed345944ba74880a13150aeea970
Johan Tan <johanaldrictan@gmail.com>
2018-11-07 16:46:08 -0800

    Started MarchingSquares portion of the project

---
+++ Assets/MeshGenerator.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshGenerator : MonoBehaviour {
+
+    public class Node
+    {
+        public Vector3 position;
+        public int vertexIndex = -1;
+
+        public Node(Vector3 _pos)
+        {
+            position = _pos;
+        }
+    }
+
+    public class ControlNode : Node
+    {
+        public bool active;
+        public Node above, right;
+
+        public ControlNode(Vector3 _pos, bool _active, float squareSize) : base(_pos)
+        {
+            active = _active;
+
+        }
+    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
